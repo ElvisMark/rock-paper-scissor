a708f210a28a0292ef1af2125c003d13953028a1
ElvisMark <52351246+ElvisMark@users.noreply.github.com>
2024-03-11 11:36:38 +0300

    Edit function play single round

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Function to get computer's choice randomly
+
 function getComputerChoice() {
     const choices = ['rock','paper','scissor'];
     const randomPick = Math.floor(Math.random() * choices.length);
     return choices[randomPick];
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+// Function to play a single round of Rock Paper Scissors
+
+function playRound (playerSelection,computerSelection) {
+
+// converts player selection to lowercase
+
+    playerSelection = playerSelection.toLowerCase();
+
+    // define winning conditions
+
+    const winningCondition = {
+        rock : 'scissor',
+        scissor : 'paper',
+        paper : 'rock'
+    };
+}
